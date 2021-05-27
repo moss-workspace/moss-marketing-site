19ba732a9d904aee404367dec6ef2d4c4b8cea63
aidensymes <hello@aidensymes.com>
2021-05-27 18:11:11 -0400

    size hero video

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,37 @@
+// Load
+////////////////////////////////////////////////////////////////////////////////
+$(document).ready(function () {
+  sizeHeroVideo();
+});
+
+// Resize
+////////////////////////////////////////////////////////////////////////////////
+$(window).resize(function () {
+  sizeHeroVideo();
+});
+
+// Scroll
+////////////////////////////////////////////////////////////////////////////////
 $(window).scroll(function () {
   $(".desktop__image").each(function (index) {
     toggleDesktopImage($(this), index);
   });
 });
 
+// Hero
+////////////////////////////////////////////////////////////////////////////////
+function sizeHeroVideo() {
+  if ($(window).height() <= $(window).width()) {
+    $("#hero__video--portrait").hide();
+    $("#hero__video--landscape").show();
+  } else {
+    $("#hero__video--portrait").show();
+    $("#hero__video--landscape").hide();
+  }
+}
+
+// Desktop
+////////////////////////////////////////////////////////////////////////////////
 function toggleDesktopImage(element, index) {
   var elementTop = element.offset().top,
     elementHeight = element.outerHeight(),
