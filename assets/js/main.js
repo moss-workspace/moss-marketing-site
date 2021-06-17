85f516e6c919f2ee5ca959a04d046ac771cf3e29
aidensymes <hello@aidensymes.com>
2021-06-17 15:01:08 -0400

    defer google analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,15 @@ $(document).ready(function () {
   });
 });
 
+// Google Analytics
+////////////////////////////////////////////////////////////////////////////////
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "UA-156611983-1");
+
 // Scroll
 ////////////////////////////////////////////////////////////////////////////////
 var lastScrollTop = 0;
