5aabf43cd11fef7ceb97b8a3c1f35aac8b05a993
aidensymes <hello@aidensymes.com>
2022-04-25 18:30:02 -0400

    refine carousel

--- assets/js/workspace.js
+++ assets/js/workspace.js
@@ -6,20 +6,26 @@ $(document).ready( function() {
       slidesToShow: 1,
       arrows: false,
       centerMode: true,
-      centerPadding: '18px',
-      autoplay: true,
+      centerPadding: '175px',
+      // autoplay: true,
       autoplaySpeed: 4000,
       pauseOnFocus: true,
       pauseOnHover: true,
       focusOnSelect: true,
+      responsive: [
+        {
+          breakpoint: 800,
+          settings: {
+            centerPadding: '26px',
+          }
+        },
+      ]
     });
 })
 
 $('.slick').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-  console.log(currentSlide);
   $('.workspace__carousel__button').removeClass('active');
   $('.workspace__carousel__button').eq(currentSlide).addClass('active');
-
 });
 
 function goToSlide(number, target) {
