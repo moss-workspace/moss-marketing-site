4dc78ede9258147c63883cc3e6d8db0f938ee732
aidensymes <hello@aidensymes.com>
2022-04-25 18:02:33 -0400

    working buttons on carousel

--- assets/js/workspace.js
+++ assets/js/workspace.js
@@ -2,21 +2,26 @@ $(document).ready( function() {
     // run slick
     $('.slick').slick({
       infinite: true,
+      initialSlide: 2,
       slidesToShow: 1,
       arrows: false,
       centerMode: true,
       centerPadding: '18px',
       autoplay: true,
       autoplaySpeed: 4000,
-    //   slidesToScroll: 3,
-      responsive: [
-        // {
-        //   breakpoint: 450,
-        //   settings: {
-        //     slidesToShow: 1,
-        //     slidesToScroll: 1
-        //   }
-        // }
-      ]
+      pauseOnFocus: true,
+      pauseOnHover: true,
+      focusOnSelect: true,
     });
 })
+
+$('.slick').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+  console.log(currentSlide);
+  $('.workspace__carousel__button').removeClass('active');
+  $('.workspace__carousel__button').eq(currentSlide).addClass('active');
+
+});
+
+function goToSlide(number, target) {
+  $('.slick').slick('slickGoTo', number);
+}
