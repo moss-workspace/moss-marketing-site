2bb9b96b27ba54ddef6113e1a351de2c90d194dd
aidensymes <hello@aidensymes.com>
2022-04-26 15:13:15 -0400

    basic sticky illustration

--- assets/js/workspace.js
+++ assets/js/workspace.js
@@ -5,7 +5,7 @@ $(document).ready( function() {
       slidesToShow: 1,
       arrows: false,
       centerMode: true,
-      centerPadding: '175px',
+      centerPadding: '300px',
       // autoplay: true,
       autoplaySpeed: 4000,
       pauseOnFocus: true,
@@ -25,11 +25,6 @@ $(document).ready( function() {
 $('.slick').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
   $('.workspace__carousel__button').removeClass('active');
   $('.workspace__carousel__button').eq(nextSlide).addClass('active');
-  $(slick.$slides).removeClass('active');
-});
-
-$('.slick').on('afterChange', function(event, slick, currentSlide, nextSlide) {
-  $(slick.$slides.get(currentSlide)).addClass('active');
 });
 
 function goToSlide(number, target) {
