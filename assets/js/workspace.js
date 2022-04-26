fa0a52ef582712fd17e21b98ad832b2408c9cb8e
aidensymes <hello@aidensymes.com>
2022-04-26 11:05:26 -0400

    refined carousel

--- assets/js/workspace.js
+++ assets/js/workspace.js
@@ -2,7 +2,6 @@ $(document).ready( function() {
     // run slick
     $('.slick').slick({
       infinite: true,
-      initialSlide: 2,
       slidesToShow: 1,
       arrows: false,
       centerMode: true,
@@ -26,6 +25,11 @@ $(document).ready( function() {
 $('.slick').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
   $('.workspace__carousel__button').removeClass('active');
   $('.workspace__carousel__button').eq(currentSlide).addClass('active');
+  $(slick.$slides).removeClass('active');
+});
+
+$('.slick').on('afterChange', function(event, slick, currentSlide, nextSlide) {
+  $(slick.$slides.get(currentSlide)).addClass('active');
 });
 
 function goToSlide(number, target) {
